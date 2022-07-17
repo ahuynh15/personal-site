9d138ae10f1a988cf8aa5b983de0e011dc646a3c
Alvin Huynh <alvin.huynh.01@gmail.com>
2022-07-17 18:13:53 -0400

    fixing scrolling

--- src/components/EducationPageWrapper/EducationPageWrapper.js
+++ src/components/EducationPageWrapper/EducationPageWrapper.js
@@ -3,12 +3,12 @@ import { EducationList } from './EducationList';
 
 const EducationPageWrapper = () => {
   return (
-    <div className="flex h-full flex-col items-center px-8 pt-8 xl:flex-row">
+    <div className="flex h-full flex-col items-center px-8 xl:flex-row">
       <div className="xl:w-1/3">
         <Title>I have completed....</Title>
       </div>
-      <div className="relative mt-8 h-full border-t-2 border-black xl:mt-0 xl:w-2/3">
-        <div className="h-full overflow-y-auto p-4">
+      <div className="relative mt-8 flex h-full overflow-y-auto xl:mt-0 xl:w-2/3">
+        <div className="p-4 xl:my-auto">
           <EducationList />
         </div>
       </div>
